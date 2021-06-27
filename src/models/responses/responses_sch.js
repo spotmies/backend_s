3cdc5c53e97b8768c9b06aff629e056f9f772e84
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-06-27 22:44:55 +0530

    responses schema updated with pdetails udetails

--- src/models/responses/responses_sch.js
+++ src/models/responses/responses_sch.js
@@ -25,6 +25,18 @@ const newReponseSch = {
     immutable: true,
     ref: "orders",
   },
+  pDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    immutable: true,
+    ref: "partners",
+  },
+  uDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    immutable: true,
+    ref: "users",
+  },
   join: timeStamp,
 
   //add join attribute hehre
