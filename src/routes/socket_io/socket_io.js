470c6b45f8c30210f6a9f1e7b949d74235504c4f
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-09-02 22:25:33 +0530

    final read reciept

--- src/routes/socket_io/socket_io.js
+++ src/routes/socket_io/socket_io.js
@@ -136,20 +136,17 @@ function updateMsgStatesAndCountsInDb(data) {
   let msgId = data.msgId;
   let status = data.status;
   let updateBlock = {}
-  console.log('data',data)
   if(data.sender === "user"){
-    console.log("user")
     if(status===3){
       updateBlock.uCount = 0 
     }
-    updateBlock.uState = status;
+    updateBlock.pState = status;
   }
   else if(data.sender === "partner"){
-     console.log("partner")
       if(status===3){
       updateBlock.pCount = 0 
     }
-    updateBlock.pState = status;
+    updateBlock.uState = status;
   }
   try {
     
