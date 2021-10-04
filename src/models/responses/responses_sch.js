e36e2419a0c03bf07f639d317dc178345c7da8f4
modernsilpi <modernsilpi@gmail.com>
2021-10-04 07:41:51 +0530

    orderState field added in responses

--- src/models/responses/responses_sch.js
+++ src/models/responses/responses_sch.js
@@ -57,6 +57,12 @@ const newReponseSch = {
     type: Boolean,
     default: false,
   },
+  orderState: {
+    type: Number,
+    required: false,
+    enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    default: 0,
+  },
 
   //add join attribute hehre
 };
