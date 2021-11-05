7130352a344d547157685274d77fa55a587754c8
modernsilpi <modernsilpi@gmail.com>
2021-11-05 08:22:57 +0530

    preparing schema for tutorials

---
+++ src/models/tutorials/schemademo.js
@@ -0,0 +1,81 @@
+var array;
+array = [
+  {
+    //unit 1
+    unitName: "Unit 1",
+    topics: [
+      {
+        topicName: "topic1",
+        data: {
+          media: [
+            {
+              mediaType: "video",
+              mediaUrl: "http://www.youtube.com/embed/dQw4w9WgXcQ",
+            },
+            {
+              mediaType: "image",
+              mediaUrl: "http://www.google.com/images/srpr/logo3w.png",
+            },
+          ],
+          content: "This is the first topic of unit 1",
+        },
+      },
+      {
+        topicName: "topic2",
+        data: {
+          media: [
+            {
+              mediaType: "video",
+              mediaUrl: "http://www.youtube.com/embed/dQw4w9WgXcQ",
+            },
+            {
+              mediaType: "image",
+              mediaUrl: "http://www.google.com/images/srpr/logo3w.png",
+            },
+          ],
+          content: "This is the second topic of unit 1",
+        },
+      },
+      
+    ],
+  },
+  {
+    //unit 2
+    unitName: "Unit 2",
+    topics: [
+      {
+        topicName: "topic1",
+        data: {
+          media: [
+            {
+              mediaType: "video",
+              mediaUrl: "http://www.youtube.com/embed/dQw4w9WgXcQ",
+            },
+            {
+              mediaType: "image",
+              mediaUrl: "http://www.google.com/images/srpr/logo3w.png",
+            },
+          ],
+          content: "This is the first topic of unit 2",
+        },
+      },
+      {
+        topicName: "topic2",
+        data: {
+          media: [
+            {
+              mediaType: "video",
+              mediaUrl: "http://www.youtube.com/embed/dQw4w9WgXcQ",
+            },
+            {
+              mediaType: "image",
+              mediaUrl: "http://www.google.com/images/srpr/logo3w.png",
+            },
+          ],
+          content: "This is the second topic of unit 2",
+        },
+      },
+      
+    ],
+  },
+];
