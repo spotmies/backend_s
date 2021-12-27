5ca99270ab9ebd7e403bd97d217d0125902eea24
modernsilpi <modernsilpi@gmail.com>
2021-12-27 19:29:19 +0530

    working on FAQ apis

--- src/routes/support/faq.js
+++ src/routes/support/faq.js
@@ -22,74 +22,129 @@ router.post("/new-faq", (req, res) => {
 /* ------------------------------- Get all faq ------------------------------ */
 
 router.get("/all-faqs", (req, res) => {
-    const isDeleted = req.query.isDeleted ?? false;
-    const isActive = req.query.isActive ?? true;
-    try {
-        db.find(
-            { isDeleted: isDeleted, isActive: isActive },
-            (err, data) => {
-                return processRequest(err, data, res);
-            }
-        );
-    } catch (error) {
-        return catchFunc(error, res);
-    }
+  const isDeleted = req.query.isDeleted ?? false;
+  const isActive = req.query.isActive ?? true;
+  try {
+    db.find({ isDeleted: isDeleted, isActive: isActive }, (err, data) => {
+      return processRequest(err, data, res);
+    });
+  } catch (error) {
+    return catchFunc(error, res);
+  }
 });
 
 /* ------------------------------ Get faq by id ----------------------------- */
 
 router.get("/faqs/:id", (req, res) => {
-    const id = req.params.id;
-    try {
-        db.findById(id, (err, data) => {
-            return processRequest(err, data, res);
-        });
-    } catch (error) {
-        return catchFunc(error, res);
-    }
+  const id = req.params.id;
+  try {
+    db.findById(id, (err, data) => {
+      return processRequest(err, data, res);
+    });
+  } catch (error) {
+    return catchFunc(error, res);
+  }
 });
 
 /* ---------------------------- Update faq by id ---------------------------- */
 
 router.put("/faqs/:id", (req, res) => {
-    const id = req.params.id;
-    let body = req.body;
-    body.lastModified = body.lastModified ?? new Date().valueOf();
-    try {
-        //set only few fields
-        db.findByIdAndUpdate(
-            id,
-            { $set: body },
-            { new: true },
-            (err, data) => {
-                return processRequest(err, data, res);
-            }
-        );
-    } catch (error) {
-        return catchFunc(error, res);
-    }
+  const id = req.params.id;
+  let body = req.body;
+  body.lastModified = body.lastModified ?? new Date().valueOf();
+  try {
+    //set only few fields
+    db.findByIdAndUpdate(id, { $set: body }, { new: true }, (err, data) => {
+      return processRequest(err, data, res);
+    });
+  } catch (error) {
+    return catchFunc(error, res);
+  }
 });
 
-/* ------------------------- Add new question to faq ------------------------ */
-router.put("/")
+/* ------------------------- Delete question from faq ------------------------ */
+
+router.delete("/question-to-faq/:id", (req, res) => {
+  const id = req.params.id;
+  const questionId = req.body.id;
+  try {
+    db.findByIdAndUpdate(
+      id,
+      { $pull: { body: { _id: questionId } } },
+      { new: true },
+      (err, data) => {
+        return processRequest(err, data, res);
+      }
+    );
+  } catch (error) {
+    return catchFunc(error, res);
+  }
+});
+
+/* ------------------------ Add new questions to faq ------------------------ */
+
+router.put("/question-to-faq/:id", (req, res) => {
+  const id = req.params.id;
+  let body = req.body;
+  body.lastModified = body.lastModified ?? new Date().valueOf();
+  const payLoad = body.payLoad;
+
+  try {
+    db.findByIdAndUpdate(
+      id,
+      { $push: { body: payLoad }, $set: { lastModified: body.lastModified } },
+      { new: true },
+      (err, data) => {
+        return processRequest(err, data, res);
+      }
+    );
+  } catch (error) {
+    return catchFunc(error, res);
+  }
+});
+
+/* ----------------------- Update object inside a FAQ ----------------------- */
+
+//need to work on this one
+router.put("/update-question-in-faq/:id/:objId", (req, res) => {
+  const id = req.params.id;
+  const objId = req.params.objId;
+  let body = req.body;
+  body.lastModified = body.lastModified ?? new Date().valueOf();
+  db.findById(id,(err,data)=>{
+    //   if(err){
+    //       return processRequest(err,data,res);
+    //       }
+    if(data){
+        //data.body.indexOf(objId).body = body;
+        data.body.forEach(element => {
+            if(element._id == objId){
+                element = body;
+            }
+        });
+        data.save((err,data)=>{
+            return processRequest(err,data,res);
+        });
+    }
+  })
+});
 
 /* ---------------------------- Delete faq by id ---------------------------- */
+
 router.delete("/faqs/:id", (req, res) => {
-    const id = req.params.id;
-    try {
-        db.findByIdAndUpdate(
-            id,
-            { $set: { isDeleted: true } },
-            { new: true },
-            (err, data) => {
-                return processRequest(err, data, res);
-            }
-        );
-    } catch (error) {
-        return catchFunc(error, res);
-    }
+  const id = req.params.id;
+  try {
+    db.findByIdAndUpdate(
+      id,
+      { $set: { isDeleted: true } },
+      { new: true },
+      (err, data) => {
+        return processRequest(err, data, res);
+      }
+    );
+  } catch (error) {
+    return catchFunc(error, res);
+  }
 });
-
-
 
 module.exports = router;
