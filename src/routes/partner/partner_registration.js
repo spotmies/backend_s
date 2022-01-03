45b3c37a9f531d127b733eb3300f99fc87724e1f
modernsilpi <modernsilpi@gmail.com>
2022-01-03 23:35:12 +0530

    fixed small import bug

--- src/routes/partner/partner_registration.js
+++ src/routes/partner/partner_registration.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const partnerDB = require("../../models/partner/partner_registration_sch");
-const locationDB = require("../../models/support/location");
 const constants = require("../../helpers/constants");
 const complaintR = require("./complaints");
 const {
