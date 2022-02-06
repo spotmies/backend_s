b31bfcf6e8c697bdf5f0ff7940f6423487350739
modernsilpi <modernsilpi@gmail.com>
2022-02-06 21:33:23 +0530

    chat schema modified for normal chat

--- src/models/messaging/messaging_sch.js
+++ src/models/messaging/messaging_sch.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const {
   unChangeUniqueStr,
-  timeStamp,
   unChangeStr,
   upStatesAndCounts,
   nonReqBool,
+  createdAt,
+  nonReqStr,
+  modifiedAt,
 } = require("../../helpers/schema/schemaHelp");
 
 const messaging = new mongoose.Schema(
@@ -12,18 +14,19 @@ const messaging = new mongoose.Schema(
     msgId: unChangeUniqueStr,
     msgs: { type: [String], validate: (v) => Array.isArray(v) && v.length > 0 },
     cBuild: upStatesAndCounts,
-    join: timeStamp,
-    ordId: unChangeStr,
+    join: createdAt,
+    ordId: nonReqStr,
     pId: unChangeStr,
     uId: unChangeStr,
     pState: upStatesAndCounts,
     uState: upStatesAndCounts,
     uCount: upStatesAndCounts,
     pCount: upStatesAndCounts,
-    lastModified: { type: Number, default: new Date().valueOf() },
+    lastModified: modifiedAt,
+    isNormalChat: nonReqBool,
     orderDetails: {
       type: mongoose.Schema.Types.ObjectId,
-      required: true,
+      required: false,
       immutable: true,
       ref: "orders",
     },
