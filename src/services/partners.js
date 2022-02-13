4856a766526e1c526cafe68c5a59776988712f78
sekharjavvadi <sekharatece@gmail.com>
2022-02-13 23:25:56 +0530

    bug solved in push rating in service methods

--- src/services/partners.js
+++ src/services/partners.js
@@ -1,15 +1,24 @@
 const pDB = require("../models/partner/partner_registration_sch");
 
 function pushRatingsToPartner(pId, reviewDoc) {
+  console.log("pushRatingsToPartner", pId, reviewDoc);
   try {
-    pDB.findOneAndUpdate({ pId: pId }, { $push: { rate: reviewDoc } });
+    pDB.findOneAndUpdate(
+      { pId: pId },
+      { $push: { rate: reviewDoc } },
+      (err, data) => {}
+    );
   } catch (error) {
     console.log(error);
   }
 }
 function pushOrdIdToPartner(pId, orderDocId) {
   try {
-    pDB.findOneAndUpdate({ pId: pId }, { $push: { orders: orderDocId } });
+    pDB.findOneAndUpdate(
+      { pId: pId },
+      { orders: { $push: orderDocId } },
+      (err, data) => {}
+    );
   } catch (error) {
     console.log(error);
   }
