d94c4964be4b7a46dc098e8f2a4eb0229ed3d989
sekharjavvadi <sekharatece@gmail.com>
2022-04-22 12:01:08 +0530

    notification sendnotification by uid implemented

---
+++ src/services/users.js
@@ -0,0 +1,26 @@
+const {
+  notificationByToken,
+} = require("../routes/firebase_admin/firebase_admin");
+const userDB  = require("../models/users/userSch");
+
+function sendNotificationByUid(uId, title, body) {
+  try {
+    userDB.findOne({ uId: uId }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(data.partnerDeviceToken);
+        notificationByToken({
+          token: data.partnerDeviceToken,
+          title: title,
+          body: body,
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+module.exports = {
+  sendNotificationByUid,
+};
